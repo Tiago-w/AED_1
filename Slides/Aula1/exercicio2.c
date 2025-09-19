3cd0ba6a51c552befd5a256409ad60d06f075225
Tiago <xoxinho2021@gmail.com>
2025-09-19 16:28:36 -0300

    codigo pronto

--- Slides/Aula1/exercicio2.c
+++ Slides/Aula1/exercicio2.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int menu(int){
-    int c;
+
+
+int menu() {
+     int c;
 
     do{
         printf("Digite uma das opcoes abaixo\n");
@@ -15,32 +19,75 @@ int menu(int){
 
     return c;
 }
-void adicionarnome(void){
-    
-    char nomes[300];
 
+void adicionarnome(char lista[][100], int *total) {
+  
     printf("Digite o nome: ");
-    scanf("%s", nomes);
+    fgets(lista[*total], 100, stdin);
     
-}
-int main(){
-    int opcao;
-    opcao=menu;
+    lista[*total][strcspn(lista[*total], "\n")] = 0;
 
-    switch (opcao)
-    {
-    case 1:
-        adicionarnome();
-        break;
-    case 2:
-        removernome();
-        break;
-    case 3:
-        listarnomes();
-        break;
-    case 4:
-        exit(0);
+    (*total)++;
+    printf("Nome adicionado.\n");
+}
+
+void removernome(char lista[][100], int *total) {
+    char nome_remover[100];
+    int indice_encontrado = -1;
+
+    printf("Digite o nome a ser removido: ");
+
+    fgets(nome_remover, sizeof(nome_remover), stdin);
+    nome_remover[strcspn(nome_remover, "\n")] = 0;
+
+    for (int i = 0; i < *total; i++) {
+        if (strcmp(lista[i], nome_remover) == 0) {
+            indice_encontrado = i;
+            break;
+        }
     }
+
+    if (indice_encontrado != -1) {
+        for (int i = indice_encontrado; i < *total - 1; i++) {
+            strcpy(lista[i], lista[i + 1]);
+        }
+        (*total)--;
+        printf("Nome removido.\n");
+    } else {
+        printf("Nome nao encontrado.\n");
+    }
+}
+
+void listarnomes(char lista[][100], int total) {
+        
+    printf("\nLista de Nomes\n");
+
+        for (int i = 0; i < total; i++) {
+            printf("%s\n", lista[i]);
+        }
+    }
+
+int main() {
+    char lista_de_nomes[50][100];
+    int total_nomes = 0;
+    int opcao;
+
+    do {
+        opcao = menu();
+        switch (opcao) {
+            case 1:
+                adicionarnome(lista_de_nomes, &total_nomes);
+                break;
+            case 2:
+                removernome(lista_de_nomes, &total_nomes);
+                break;
+            case 3:
+                listarnomes(lista_de_nomes, total_nomes);
+                break;
+            case 4:
+
+        }
+    } while (opcao);
 
     return 0;
 }
