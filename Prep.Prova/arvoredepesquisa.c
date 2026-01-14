1454014c507242e8d87967777680f24479d1ebe8
Tiago <xoxinho2021@gmail.com>
2026-01-13 21:14:44 -0300

    implementação da remoção da arvore de pesquisa

--- Prep.Prova/arvoredepesquisa.c
+++ Prep.Prova/arvoredepesquisa.c
@@ -34,18 +34,58 @@ void imprimirOrdemCentral( struct no* raiz ) {
 		imprimirOrdemCentral( raiz->direita );
 	}
 }
+struct no* encontrarMenor( struct no* raiz ) {
+	while( raiz->esquerda != NULL ) {
+		raiz = raiz->esquerda;
+	}
+	return raiz;
+}
+
+struct no* remover( struct no* raiz, int valor ) {
+	if( raiz == NULL ) {
+		printf( "valor nao encontrado\n" );
+		return NULL;
+	}
+
+	if( valor < raiz->valor ) {	
+		raiz->esquerda = remover( raiz->esquerda, valor );
+	} else if( valor > raiz->valor ) {
+		raiz->direita = remover( raiz->direita, valor );   //procura o valor que queremos remover
+	} else {
+		if( raiz->esquerda == NULL && raiz->direita == NULL ) {  //se é folha, só remove a raiz
+			free( raiz );
+			printf( "elemento removido\n" );
+			return NULL;
+		} else if( raiz->esquerda == NULL ) {           //se tem algo no lado direito e nao no esquerdo
+			struct no* aux = raiz->direita;				//então coloca o valor do lado direito em uma variavel temporaria
+			free( raiz );								//e tira esse nó
+			printf( "elemento removido\n" );
+			return aux;
+		} else if( raiz->direita == NULL ) {           //se nao tem no lado direito e tem no lado esquerdo, az a mesma coisa
+			struct no* aux = raiz->esquerda;			//coloca esse valor numa variavel temporaria e tira o nó e retorna 
+			free( raiz );								//o valor temporario
+			printf( "elemento removido\n" );
+			return aux;
+		} else {
+			struct no* sucessor = encontrarMenor( raiz->direita );      //se é um nó que tem dois filhos, precisamos achar o menor, n entendi
+			raiz->valor			= sucessor->valor;
+			raiz->direita		= remover( raiz->direita, sucessor->valor );
+		}
+	}
+	return raiz;
+}
 
 int main() {
 
 	struct no* raiz = NULL;
-	int		   num;
+	int		   num, op;
 
-    printf("Digite a raiz ( numero ): ");
+	printf( "Digite a raiz ( numero ): " );
 	scanf( "%d", &num );
 	raiz = inserir( raiz, num );
-    printf("Digite o restante dos numeros ( digite 0 para sair ): \n");
+	printf( "digite o restante dos numeros ( digite 0 para sair ): \n" );
 	do {
-        
+
 		scanf( "%d", &num );
 		if( num == 0 ) {
 			break;
@@ -53,9 +93,22 @@ int main() {
 		inserir( raiz, num );
 	} while( num != 0 );
 
-	printf( "Numeros da arvore em ordem crescente:\n" );
+	printf( "numeros da arvore em ordem crescente:\n" );
 	imprimirOrdemCentral( raiz );
 	printf( "\n" );
+
+	do {
+		printf( "\ndigite 1 para remover um número ou 2 para sair" );
+		scanf( "%d", &op );
+		if( op == 1 ) {
+			printf( "digite o número a remover: " );
+			scanf( "%d", &num );
+			raiz = remover( raiz, num );
+			printf( "arvore atual: " );
+			imprimirOrdemCentral( raiz );
+			printf( "\n" );
+		}
+	} while( op == 1 );
 
 	return 0;
 }
